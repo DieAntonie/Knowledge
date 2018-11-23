043ae1c54d8071f1524dc0ed81573f9ee8f3cb84
Chris Antonie Pieterse <Chrisantoniep@gmail.com>
2018-11-23 14:32:33 +0200

    Initial base commit

---
+++ Controllers/main_controller.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/**
+ * Main Epistemology controller. 
+ * @param {Array} options 
+ */
+export default function main(options) {
+    alert("Main Controller is working!");
+    return this;
+}
